f970a0c97dda69255ced81a23a392d4099386ce2
Tuyen <thetuyen124@gmail.com>
2021-09-19 18:32:16 +0700

    first commit

---
+++ foody-web-service/src/test/java/com/tlu/datn/foody/FoodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlu.datn.foody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ foody-web-ui/src/App.js
@@ -0,0 +1,6 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">app</div>;
+}
+export default App;
